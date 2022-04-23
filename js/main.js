cb88e3a9e0f836fa38acc41b55bb8e16f22f512e
TysonReese <ree18003@byui.edu>
2022-04-22 21:08:31 -0600

    Updated

--- js/main.js
+++ js/main.js
@@ -2,5 +2,57 @@ const links = [
     {
       label: "Week1 notes",
       url: "week1/index.html"
-    }
+    },
+    {
+        label: "Week2 notes",
+        url: "week2/index.html"
+      },
+      {
+        label: "Week3 notes",
+        url: "week3/index.html"
+      },
+      {
+        label: "Week4 notes",
+        url: "week4/index.html"
+      },
+      {
+        label: "Week5 notes",
+        url: "week5/index.html"
+      },
+      {
+        label: "Week6 notes",
+        url: "week6/index.html"
+      },
+      {
+        label: "Week7 notes",
+        url: "week7/index.html"
+      },
+      {
+        label: "Week8 notes",
+        url: "week8/index.html"
+      },
+      {
+        label: "Week9 notes",
+        url: "week9/index.html"
+      },
+      {
+        label: "Week10 notes",
+        url: "week10/index.html"
+      },
+      {
+        label: "Week11 notes",
+        url: "week11/index.html"
+      },
+      {
+        label: "Week12 notes",
+        url: "week12/index.html"
+      },
+      {
+        label: "Week13 notes",
+        url: "week13/index.html"
+      },
+      {
+        label: "Week14 notes",
+        url: "week14/index.html"
+      }
   ]
